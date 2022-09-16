467c39e6cb4d5742ad06e1ac4575e2ae0937aa80
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-09-16 08:27:06 -0300

    fix: changing project configuration

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/HomeController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using CleanArchMvc.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
