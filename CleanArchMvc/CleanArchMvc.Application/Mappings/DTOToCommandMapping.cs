d122f49834482be1e497208886eb82a2cb760f4d
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-09-19 21:59:30 -0300

    refactor: remove duplicated logic

---
+++ CleanArchMvc/CleanArchMvc.Application/Mappings/DTOToCommandMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Products.Commands;
+
+namespace CleanArchMvc.Application.Mappings
+{
+        public class DTOToCommandMapping : Profile
+        {
+            public DTOToCommandMapping()
+            {
+                CreateMap<ProductDTO, ProductCreateCommand>().ReverseMap();
+                CreateMap<ProductDTO, ProductUpdateCommand>().ReverseMap();
+
+            }
+        }
+}
