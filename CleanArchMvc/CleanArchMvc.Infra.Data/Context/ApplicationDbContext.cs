32f507516b0be6974bca3c2d62f6382eb62cec06
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-09-21 07:45:49 -0300

    feat: add Authtentication Identity

--- CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using CleanArchMvc.Domain.Entities;
-using CleanArchMvc.Infra.Data.EntitiesConfiguration;
+using CleanArchMvc.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -13,10 +14,10 @@ namespace CleanArchMvc.Infra.Data.Context
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
     }
 }
